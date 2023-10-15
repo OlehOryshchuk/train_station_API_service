2b6229d38d5d49e8114baf497c3bc9505d02b489
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-15 19:59:52 +0300

    chore: Set commenst for User views

--- user/views.py
+++ user/views.py
@@ -7,10 +7,12 @@ from .serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
+    """Create and save user in Database User table"""
     serializer_class = UserSerializer
 
 
 class ManagerUserView(generics.RetrieveUpdateAPIView):
+    """Return current user and authentication is required"""
     serializer_class = UserSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)

3f1aa5501113d5d18c4685fe49bed702fd9ee828
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-15 19:40:19 +0300

    Set URL paths for registering and retrieving/updating your user

--- user/urls.py
+++ user/urls.py
@@ -5,9 +5,17 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+from .views import (
+    CreateUserView,
+    ManagerUserView,
+)
+
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManagerUserView.as_view(), name="manage"),
 ]

2b83b458ca76a4d118294e1458a78d61ed42e0ac
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-15 17:54:52 +0300

    feat: Create UserSerializer and define create and update methods

--- user/serializers.py
+++ user/serializers.py
@@ -2,3 +2,23 @@ from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        fields = ("id", "email", "password", "is_staff")
+        read_only_fields = ("is_staff",)
+        extra_kwargs = {"password": {"write_only": True, "min_length": 10}}
+
+    def create(self, validated_data):
+        """Create new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Updated user with encrypted password and return it"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, **validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user

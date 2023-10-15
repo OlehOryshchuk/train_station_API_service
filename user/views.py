dd224efc8f76036651c0de1f5a4570b86102f47e
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-15 19:22:15 +0300

    feat: Create empty CreateUserView and ManageUserView for creating and updating user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    pass
+
+
+class ManagerUserView(generics.RetrieveUpdateAPIView):
+    pass

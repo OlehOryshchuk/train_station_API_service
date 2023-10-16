d40f40fe9604143f47360773b34974754d7006fa
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-16 18:42:14 +0300

    feat: Create model Station

--- train_station_resource/models.py
+++ train_station_resource/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class Station(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+

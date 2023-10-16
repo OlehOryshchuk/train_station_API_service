048dcdf21f56757c39dc0384560afc9c9b4bc935
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-16 19:21:43 +0300

    feat: Create Order model

--- train_station_resource/models.py
+++ train_station_resource/models.py
@@ -93,3 +93,18 @@ class Trip(models.Model):
 
     def __str__(self) -> str:
         return self.route + " " + self.departure_time
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)

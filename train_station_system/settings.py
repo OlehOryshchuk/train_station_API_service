d70d80201fa35608dd5c4a1c507588196382555a
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-19 19:01:56 +0300

    feat: Set rest_fr default_permission_classes custom permission IsAdminOrIfAuthenticatedReadOnly

--- train_station_system/settings.py
+++ train_station_system/settings.py
@@ -137,6 +137,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "train_station_resource.permissions.IsAdminOrIfAuthenticatedReadOnly"
     )
 }
 

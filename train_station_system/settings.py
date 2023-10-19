9193b98344bd872763473cb9992ac94921699d2e
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-19 18:51:45 +0300

    feat: Set defualt authentication classes JWTAuthentication

--- train_station_system/settings.py
+++ train_station_system/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
 

4641eb9eb23c162c6869743c2cb6ae1feb4d0d38
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-19 16:23:06 +0300

    feat: Register all train_station_resource/views.py

--- train_station_resource/urls.py
+++ train_station_resource/urls.py
@@ -11,3 +11,16 @@ from .views import (
     OrderViewSet,
 )
 
+router = routers.DefaultRouter()
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+router.register("train_types", TrainTypeViewSet)
+router.register("trains", TrainViewSet)
+router.register("crews", CrewViewSet)
+router.register("trips", TripViewSet)
+router.register("orders", OrderViewSet)
+
+urlpatterns = [path(""), include(router.urls)]
+
+app_name = "train_station_resource"
+

69635d64e978a4b590af1be68a8c95745b0b8c31
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-20 19:39:10 +0300

    feat: Inlcude 'debug_toolbar' in out project

--- train_station_system/settings.py
+++ train_station_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_spectacular",
+    "debug_toolbar",
     "rest_framework",
     "train_station_resource",
     "django_filters",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",

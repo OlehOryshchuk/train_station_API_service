f95c3d52649d6638eeb3322686fc5221a9f4a411
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-20 18:47:53 +0300

    feat: Inlcude URL path to documentation of our API

--- train_station_system/urls.py
+++ train_station_system/urls.py
@@ -19,12 +19,31 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("api/user/", include("user.urls", namespace="user")),
     path(
         "api/train_station/",
         include("train_station_resource.urls", namespace="train_station")
+    ),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema")
+    ),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger"
     )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

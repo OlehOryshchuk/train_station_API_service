ac5ddf39a4711dc60d63b46da02281dd3de7fe43
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-21 19:00:28 +0300

    test: Create empty 'tests/test_models.py'

---
+++ train_station_resource/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from datetime import datetime
+
+from train_station_resource.models import (
+    Station,
+    Route,
+    TrainType,
+    Crew,
+    Train,
+    Trip,
+    Order,
+    Ticket,
+)
+
+
+class ModelsTest(TestCase):
+    pass

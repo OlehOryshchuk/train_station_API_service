a4344ce073a45f9124d2e4f9126a41e7773efd46
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-21 20:01:47 +0300

    test: Create tests for model Route

--- train_station_resource/tests/test_models.py
+++ train_station_resource/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 
 from datetime import datetime
 
@@ -19,6 +20,12 @@ def sample_station(name: str) -> Station:
     return Station.objects.create(longitude=50, latitude=50, name=name)
 
 
+def sample_route(source: Station, destination: Station) -> Route:
+    return Route.objects.create(
+        source=source, destination=destination, distance=100
+    )
+
+
 class ModelsTest(TestCase):
     def test_station_string_representation(self):
         station = sample_station(name="Station1")
@@ -28,4 +35,30 @@ class ModelsTest(TestCase):
         sample_station(name="Station1")
         with self.assertRaises(IntegrityError):
             sample_station(name="Station1")
+
+    def test_route_string_rep_property(self):
+        station1 = sample_station(name="Station1")
+        station2 = sample_station(name="Station2")
+        route1 = sample_route(station1, station2)
+
+        expect_str = f"{station1} - {station2}"
+
+        self.assertEqual(route1.string_repr, expect_str)
+
+    def test_route_source_destination_are_not_same(self):
+        station1 = sample_station(name="Station1")
+        with self.assertRaises(ValidationError):
+            sample_route(station1, station1)
+
+    def test_route_source_destination_uniqueness(self):
+        station1 = sample_station(name="Station1")
+        station2 = sample_station(name="Station2")
+        sample_route(station1, station2)
+
+        with self.assertRaises(ValidationError):
+            sample_route(station1, station2)
+
+    def test_route_index_source(self):
+        indexes = Route._meta.indexes
+        self.assertEqual(indexes[0].fields[0], "source")
 

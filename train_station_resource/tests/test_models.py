01a6a43cb51bc0408f92390741bf107401859b18
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-21 21:54:35 +0300

    teat: Create tests for Train model

--- train_station_resource/tests/test_models.py
+++ train_station_resource/tests/test_models.py
@@ -14,20 +14,12 @@ from train_station_resource.models import (
     Order,
     Ticket,
 )
-
-
-def sample_station(name: str) -> Station:
-    return Station.objects.create(longitude=50, latitude=50, name=name)
-
-
-def sample_route(source: Station, destination: Station) -> Route:
-    return Route.objects.create(
-        source=source, destination=destination, distance=100
-    )
-
-
-def sample_train_type(name: str) -> TrainType:
-    return TrainType.objects.create(name=name)
+from .models_create_sample import (
+    sample_route,
+    sample_station,
+    sample_train_type,
+    sample_train,
+)
 
 
 class ModelsTest(TestCase):
@@ -78,3 +70,21 @@ class ModelsTest(TestCase):
     def test_train_type_index_source(self):
         indexes = TrainType._meta.indexes
         self.assertEqual(indexes[0].fields[0], "name")
+
+    def test_train_string_representation(self):
+        train = sample_train_type(name="Train1")
+        self.assertEqual(str(train), train.name)
+
+    def test_train_name_uniqueness(self):
+        sample_train(name="Train1")
+        with self.assertRaises(IntegrityError):
+            sample_train(name="Train1")
+
+    def test_train_capacity_property(self):
+        train = sample_train(
+            name="Train1",
+            cargo_num=10,
+            seats_in_cargo=10,
+        )
+
+        self.assertEqual(train.capacity, 100)

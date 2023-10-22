3b342d1140a0c921e06ecd8dce66607609aa5b57
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-22 12:43:09 +0300

    test: Create tests for Station admin site

--- train_station_resource/tests/test_admins.py
+++ train_station_resource/tests/test_admins.py
@@ -2,7 +2,43 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from .models_create_sample import(
+    sample_station,
+)
 
 
 class AdminTest(TestCase):
-    pass
+    def setUp(self) -> None:
+        self.admin = get_user_model().objects.create_superuser(
+            email="admin@gmail.com", password="rvtwfafhg"
+        )
+        self.client.force_login(self.admin)
+
+    def test_admin_station_site_has_require_fields(self):
+        station = sample_station(name="Station1")
+        url = reverse(
+            "admin:train_station_resource_station_changelist"
+        )
+        res = self.client.get(url)
+
+        self.assertContains(res, station.name)
+        self.assertContains(res, station.longitude),
+        self.assertContains(res, station.latitude),
+
+    def test_admin_station_search_by_name(self):
+        station1 = sample_station(name="MainStation")
+        station2 = sample_station(name="SecondStation")
+
+        url = reverse(
+            "admin:train_station_resource_station_changelist"
+        )
+
+        res = self.client.get(url, {"q": station1.name.lower()})
+
+        changelist = res.context["cl"]
+        self.assertIn(
+            station1, changelist.queryset
+        )
+        self.assertNotIn(
+            station2, changelist.queryset
+        )

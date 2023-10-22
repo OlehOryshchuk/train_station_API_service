865d8fc08eb9bf5352fb0e95834cea60854ebb9a
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-22 19:12:13 +0300

    refact: Change string repsentation of Trip model

--- train_station_resource/admin.py
+++ train_station_resource/admin.py
@@ -22,6 +22,7 @@ class StationAdmin(admin.ModelAdmin):
 class RouteAdmin(admin.ModelAdmin):
     search_fields = ["source", "destination"]
     list_filter = ["distance"]
+    list_display = ["source", "destination", "distance"]
 
 
 @admin.register(TrainType)
@@ -33,6 +34,12 @@ class TrainTypeAdmin(admin.ModelAdmin):
 class TrainAdmin(admin.ModelAdmin):
     search_fields = ["name", "train_type__name"]
     list_filter = ["cargo_num", "seats_in_cargo"]
+    list_display = [
+        "name",
+        "cargo_num",
+        "seats_in_cargo",
+        "train_type"
+    ]
 
 
 @admin.register(Crew)
@@ -44,6 +51,17 @@ class CrewAdmin(admin.ModelAdmin):
 class TripAdmin(admin.ModelAdmin):
     search_fields = ["route__source", "route__destination"]
     list_filter = ["train__train_type"]
+    list_display = [
+        "display_route_string_repr",
+        "train",
+        "departure_time",
+        "arrival_time"
+    ]
+
+    def display_route_string_repr(self, obj):
+        return obj.route.string_repr  # Reference the property
+
+    display_route_string_repr.short_description = "Route"  # Customize the column header
 
 
 class TicketInline(admin.TabularInline):
@@ -55,6 +73,23 @@ class TicketInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = [TicketInline]
     search_fields = ["user__email"]
+    list_display = [
+        "user", "created_at"
+    ]
 
 
-admin.site.register(Ticket)
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    search_fields = ["order__user"]
+    list_filter = ["trip"]
+    list_display = [
+        "cargo",
+        "seat",
+        "trip",
+        "order"
+    ]
+
+    def display_route_string_repr(self, obj):
+        return obj.route.string_repr  # Reference the property
+
+    display_route_string_repr.short_description = "Route"  # Customize the column header

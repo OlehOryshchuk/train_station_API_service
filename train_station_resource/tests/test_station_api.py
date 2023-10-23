61559c81aa8b9efdb06572acb87c7d848bd9f071
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-23 18:34:54 +0300

    test: Test Station API login required

--- train_station_resource/tests/test_station_api.py
+++ train_station_resource/tests/test_station_api.py
@@ -9,3 +9,11 @@ from train_station_resource.serializers import StationSerializer
 
 STATION_URL = reverse("train_station:station-list")
 
+
+class UnauthenticatedStationApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(STATION_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)

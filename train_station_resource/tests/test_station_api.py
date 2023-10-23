9cc4084ad0277c02987e7a8b4333dc6e5f61faed
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-23 18:30:34 +0300

    test: Create empty 'test_station_api.py' for tests

---
+++ train_station_resource/tests/test_station_api.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from train_station_resource.serializers import StationSerializer
+
+STATION_URL = reverse("train_station:station-list")
+

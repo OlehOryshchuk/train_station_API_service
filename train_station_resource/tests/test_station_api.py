4d5afafc555f92772c87201e66cde9d792133de9
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-23 19:35:58 +0300

    test: Test Station api list view in AuthenticatedStationApiTests

--- train_station_resource/tests/test_station_api.py
+++ train_station_resource/tests/test_station_api.py
@@ -6,6 +6,8 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 from train_station_resource.serializers import StationSerializer
+from .models_create_sample import sample_station
+from train_station_resource.models import Station
 
 STATION_URL = reverse("train_station:station-list")
 
@@ -17,3 +19,26 @@ class UnauthenticatedStationApiTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(STATION_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedStationApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="Main@gmail.com", password="rvtquen"
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_station(self):
+        sample_station(name="Station1")
+        sample_station(name="Station2")
+
+        res = self.client.get(STATION_URL)
+
+        stations = Station.objects.all()
+        serializers = StationSerializer(stations, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["results"], serializers.data)
+
+

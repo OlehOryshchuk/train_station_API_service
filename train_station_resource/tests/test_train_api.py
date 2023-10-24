d9b8a8488c014e139489a66b0f27b64f89718195
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-24 20:03:07 +0300

    test: Test Train api upload image, in cases where user & admin & unon

--- train_station_resource/tests/test_train_api.py
+++ train_station_resource/tests/test_train_api.py
@@ -1,3 +1,7 @@
+import tempfile
+import os
+
+from PIL import Image
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -9,16 +13,21 @@ from train_station_resource.serializers import (
     TrainSerializer,
     TrainListSerializer,
     TrainDetailSerializer,
-    TrainImageSerializer,
 )
 from .models_create_sample import (
     sample_train_type,
     sample_train,
+    sample_trip,
     detail_url
 )
 from train_station_resource.models import Train
 
 TRAIN_URL = reverse("train_station:train-list")
+TRIP_URL = reverse("train_station:trip-list")
+
+
+def upload_train_image_url(train_id: int):
+    return reverse("train_station:train-upload-image", args=[train_id])
 
 
 class UnauthenticatedTrainApiTests(TestCase):
@@ -36,6 +45,11 @@ class UnauthenticatedTrainApiTests(TestCase):
     def test_get_detail_method_auth_required(self):
         train1 = sample_train(name="Train1")
         res = self.client.get(detail_url("train", train1.id))
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_upload_image_method_auth_required(self):
+        train = sample_train(name="Train1")
+        res = self.client.get(upload_train_image_url(train.id))
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -128,12 +142,20 @@ class AuthenticatedTrainApiTests(TestCase):
         for method in forbidden_methods:
             self.assertNotIn(method, allowed_methods)
 
+    def test_user_can_not_upload_image(self):
+        train = sample_train(name="Train1")
+        url = upload_train_image_url(train.id)
+
+        res = self.client.post(url)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
 
 class AdminTrainApi(TestCase):
     def setUp(self) -> None:
         self.client = APIClient()
-        self.admin = get_user_model().objects.create_user(
-            email="test@gmai.com", password="rvtafj", is_staff=True
+        self.admin = get_user_model().objects.create_superuser(
+            email="test@gmai.com", password="rvtafj"
         )
         self.client.force_authenticate(self.admin)
 
@@ -144,6 +166,14 @@ class AdminTrainApi(TestCase):
     def test_admin_train_can_get_detail(self):
         train = sample_train(name="Train1")
         res = self.client.get(detail_url("train", train.id))
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_admin_can_upload_image(self):
+        train = sample_train(name="Train1")
+        url = upload_train_image_url(train.id)
+
+        res = self.client.post(url)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
@@ -163,3 +193,89 @@ class AdminTrainApi(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual(res.data, serializer.data)
+
+
+class TrainImageUploadTest(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.admin = get_user_model().objects.create_user(
+            email="test@gmai.com", password="rvtafj", is_staff=True
+        )
+        self.client.force_authenticate(self.admin)
+        self.train = sample_train(name="MainTrain")
+
+    def tearDown(self) -> None:
+        self.train.image.delete()
+
+    def test_upload_image_to_train(self):
+        url = upload_train_image_url(self.train.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(
+                url, {"image": ntf}, format="multipart"
+            )
+        self.train.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("image", res.data)
+        self.assertTrue(os.path.exists(self.train.image.path))
+
+    def test_upload_image_bad_request(self):
+        url = upload_train_image_url(self.train.id)
+        res = self.client.post(
+            url, {"image": "not image"}, format="multipart"
+        )
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_image_to_train_list_should_not_work(self):
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+
+            train_data = {
+                "name": "Train1",
+                "cargo_num": 10,
+                "seats_in_cargo": 20,
+                "train_type": sample_train_type(name="TrainType1").id,
+                "image": ntf
+            }
+            res = self.client.post(
+                TRAIN_URL, train_data, format="multipart"
+            )
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        train = Train.objects.get(name=train_data["name"])
+        self.assertFalse(train.image)
+
+    def test_image_url_is_shown_on_list(self):
+        url = upload_train_image_url(self.train.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(
+                url, {"image": ntf}, format="multipart"
+            )
+        res = self.client.get(TRAIN_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("image", res.data["results"][0].keys())
+
+    def test_image_url_is_shown_on_detail(self):
+        url = upload_train_image_url(self.train.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(
+                url, {"image": ntf}, format="multipart"
+            )
+        sample_trip(name="Trip1", train=self.train)
+        res = self.client.get(TRIP_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("train_image", res.data["results"][0].keys())

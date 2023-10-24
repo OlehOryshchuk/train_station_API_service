f0e2c25cc3315b8196b7cd6d07ffe78badafda69
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-24 17:32:58 +0300

    test: Test TrainType api where user is admin

--- train_station_resource/tests/test_train_type_api.py
+++ train_station_resource/tests/test_train_type_api.py
@@ -9,7 +9,7 @@ from train_station_resource.serializers import TrainTypeSerializer
 from .models_create_sample import sample_train_type
 from train_station_resource.models import TrainType
 
-STATION_URL = reverse("train_station:traintype-list")
+TRAIN_TYPE_URL = reverse("train_station:traintype-list")
 
 
 class UnauthenticatedTrainTypeApiTests(TestCase):
@@ -17,11 +17,11 @@ class UnauthenticatedTrainTypeApiTests(TestCase):
         self.client = APIClient()
 
     def test_get_method_auth_required(self):
-        res = self.client.get(STATION_URL)
+        res = self.client.get(TRAIN_TYPE_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_post_method_auth_required(self):
-        res = self.client.post(STATION_URL)
+        res = self.client.post(TRAIN_TYPE_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -37,7 +37,7 @@ class AuthenticatedTrainTypeApiTests(TestCase):
         sample_train_type(name="TrainType1")
         sample_train_type(name="TrainType2")
 
-        res = self.client.get(STATION_URL)
+        res = self.client.get(TRAIN_TYPE_URL)
 
         stations = TrainType.objects.all()
         serializers = TrainTypeSerializer(stations, many=True)
@@ -46,12 +46,12 @@ class AuthenticatedTrainTypeApiTests(TestCase):
         self.assertEqual(res.data["results"], serializers.data)
 
     def test_create_train_type_forbidden(self):
-        res = self.client.post(STATION_URL)
+        res = self.client.post(TRAIN_TYPE_URL)
 
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_train_type_api_has_only_get_and_post_methods(self):
-        res = self.client.post(STATION_URL)
+        res = self.client.post(TRAIN_TYPE_URL)
         allowed_methods = res.headers["Allow"]
         forbidden_methods = ["PATCH", "PUT", "DELETE"]
 
@@ -60,3 +60,27 @@ class AuthenticatedTrainTypeApiTests(TestCase):
         for method in forbidden_methods:
             self.assertNotIn(method, allowed_methods)
 
+
+class AdminTrainTypeApi(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.admin = get_user_model().objects.create_user(
+            email="test@gmai.com", password="rvtafj", is_staff=True
+        )
+        self.client.force_authenticate(self.admin)
+
+    def test_admin_can_make_get_request(self):
+        res = self.client.get(TRAIN_TYPE_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_admin_can_create_train_type(self):
+        train_type_data = {
+            "name": "TrainType1",
+        }
+        res = self.client.post(TRAIN_TYPE_URL, train_type_data)
+
+        station1 = TrainType.objects.get(name=train_type_data["name"])  # Retrieve the specific instance
+        serializer = TrainTypeSerializer(station1)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data, serializer.data)

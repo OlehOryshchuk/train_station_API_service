17a6494811cc0603c0d8d7cd6f5734476fa78d52
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-24 16:59:45 +0300

    test: Test Route Api, for authenticated users

--- train_station_resource/tests/test_route_api.py
+++ train_station_resource/tests/test_route_api.py
@@ -26,3 +26,40 @@ class UnauthenticatedRouteApiTests(TestCase):
     def test_post_method_auth_required(self):
         res = self.client.post(ROUTE_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedRouteApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="Main@gmail.com", password="rvtquen"
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_route(self):
+        station1 = sample_station(name="Station1")
+        station2 = sample_station(name="Station2")
+        sample_route(station1, station2)
+        sample_route(station2, station1)
+
+        res = self.client.get(ROUTE_URL)
+
+        routes = Route.objects.all()
+        serializers = RouteListSerializer(routes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["results"], serializers.data)
+
+    def test_create_station_forbidden(self):
+        res = self.client.post(ROUTE_URL)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_station_api_has_only_get_and_post_methods(self):
+        res = self.client.post(ROUTE_URL)
+        allowed_methods = res.headers["Allow"]
+        forbidden_methods = ["PATCH", "PUT", "DELETE"]
+
+        self.assertIn("GET", allowed_methods)
+        self.assertIn("POST", allowed_methods)
+        for method in forbidden_methods:
+            self.assertNotIn(method, allowed_methods)

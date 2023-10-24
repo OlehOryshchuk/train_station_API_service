dd8161f05f374491f50f215c84c629590b2f2adb
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-24 16:52:08 +0300

    test: Test Station api create authenticated require

--- train_station_resource/tests/test_station_api.py
+++ train_station_resource/tests/test_station_api.py
@@ -17,8 +17,12 @@ class UnauthenticatedStationApiTests(TestCase):
     def setUp(self) -> None:
         self.client = APIClient()
 
-    def test_auth_required(self):
+    def test_get_method_auth_required(self):
         res = self.client.get(STATION_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_method_auth_required(self):
+        res = self.client.post(STATION_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 

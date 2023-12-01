1ecd5bd661b459b38034ccfb16d6e03f002885c4
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-01 20:34:58 +0200

    refact: Allow any user to register

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
@@ -9,6 +9,7 @@ from .serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create and save user in Database User table"""
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class ManagerUserView(generics.RetrieveUpdateAPIView):
